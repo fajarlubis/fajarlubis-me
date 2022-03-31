e031b24890625230a78b5305bcb6406458c1ca58
Fajar Lubis <fajar@ddtc.co.id>
2022-03-31 11:27:16 +0700

    remove default nuxt template

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <Tutorial />
+  <div>
+    <h1>Hehe</h1>
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/AfxsFsLhL04"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  </div>
 </template>
 
 <script>
