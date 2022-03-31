a54b34e39cdc76c5765861ab3b2742e08192e9e4
Fajar Lubis <fajar@ddtc.co.id>
2022-03-31 11:39:54 +0700

    add unnecessary things

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
     <h1>Hehe</h1>
+    <h2>
+      My LinkedIn
+      <a href="https://www.linkedin.com/in/fajarlubis/" target="_blank"
+        >CLICK HERE</a
+      >
+    </h2>
     <iframe
       width="560"
       height="315"
       src="https://www.youtube.com/embed/AfxsFsLhL04"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/WFsAon_TWPQ"
       title="YouTube video player"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
