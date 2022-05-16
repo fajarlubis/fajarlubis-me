d56df0df6a44d77d1f8a32ef29677ab25fb736f5
Fajar Lubis <fajar@ddtc.co.id>
2022-05-16 23:16:48 +0700

    46fa9e3

---
+++ _nuxt/static/1652717802/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffajarlubis-me\u002F_nuxt\u002Fstatic\u002F1652717802",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ffajarlubis-me\u002F",assetsPath:"\u002Ffajarlubis-me\u002F_nuxt\u002F",cdnURL:a}}}}(null));
